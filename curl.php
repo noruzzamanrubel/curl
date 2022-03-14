472819bc77ee202351483b87321e24675bf27d0c
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-03-14 12:46:25 +0600

    fix sentex error

--- curl.php
+++ curl.php
@@ -2,6 +2,6 @@
 
 echo "Welcome to the beautiful world of CURL and PHP!";
 
-if(isset($_POST['secret'])) && 'noodles' == $_POST['secret']){
+if(isset($_POST['secret']) && 'noodles' == $_POST['secret']){
     echo "<br/>please have some noodles:)";
 }

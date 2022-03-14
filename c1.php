9dfa3c100da6ba757efeaa0a0e9894300f22b235
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-03-14 13:20:14 +0600

    f

--- c1.php
+++ c1.php
@@ -1,2 +1,7 @@
 <?php 
 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://localhost/curl/curl.php');
+curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'POST' );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, 'secret=noodles' );
+curl_exec($ch);
---
+++ c2.php
@@ -0,0 +1,3 @@
+<?php
+$ch = curl_init('https://simplenote.com/');
+curl_exec($ch);
